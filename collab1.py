c15e26f2c6cb6728264aceccacf93edfc10880c2
Priyanshi Kalra <priyanshikalra718@gmail.com>
2025-03-17 10:50:12 +0530

    commit on collab1

--- collab1.py
+++ collab1.py
@@ -1,11 +1,11 @@
 # This program adds two numbers
 
-num1 = 1.5
-num2 = 6.3
+n1= 1
+n2= 6
 
 # Add two numbers
-sum = num1 + num2
+sum = n1 + n2
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(n1, n2, sum))
 
